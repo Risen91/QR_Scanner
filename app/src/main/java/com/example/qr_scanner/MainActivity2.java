8aaa7ff8ac7d1508769f92f3390065d6966cabd8
Risen91 <you@example.comAbax3@hotmail.com>
2022-08-04 17:37:39 +0200

    Added QR Scanner

--- app/src/main/java/com/example/qr_scanner/MainActivity2.java
+++ app/src/main/java/com/example/qr_scanner/MainActivity2.java
@@ -1,28 +1,70 @@
 package com.example.qr_scanner;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class MainActivity2 extends AppCompatActivity {
     private Button button_mainMenu;
+    private Button btn_scan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        button_mainMenu = (Button) findViewById(R.id.button_mainMenu);
+        button_mainMenu =findViewById(R.id.button_mainMenu);
+        btn_scan =findViewById(R.id.btn_scan);
+
         button_mainMenu.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openActivity();
             }
         });
+
+
+        btn_scan.setOnClickListener(v->
+        {
+            scanCode();
+        });
+
     }
+
+    private void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(),result ->
+    {
+        if(result.getContents() !=null)
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity2.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener()
+            {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i)
+                {
+                    dialogInterface.dismiss();
+                }
+            }).show();
+        }
+    });
 
     public void openActivity() {
         Intent intent = new Intent(this, MainActivity.class);
